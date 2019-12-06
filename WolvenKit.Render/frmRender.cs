305e39a0cd310e1d9f7b8b37375f18098c770dde
Stephen White <stephenjeffreywhite@outlook.com>
2019-12-06 03:40:54 -0500

    Added TODO

--- WolvenKit.Render/frmRender.cs
+++ WolvenKit.Render/frmRender.cs
@@ -14,6 +14,7 @@ using System.Collections.Generic;
 
 namespace WolvenKit.Render
 {
+    //TODO - Convert to XtraUserControl
     public partial class frmRender : DockContent
     {
         /// <summary>
