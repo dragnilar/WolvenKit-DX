187f98a3a06bafe4da15de2eb47f4c7dc455621a
Stephen White <stephenjeffreywhite@outlook.com>
2019-12-15 12:17:18 -0500

    Moved variable list node to CR2W project

---
+++ WolvenKit.CR2W/VariableListNode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WolvenKit.CR2W.Editors;
+
+namespace WolvenKit.CR2W
+{
+    public class VariableListNode
+    {            public string Name
+            {
+                get
+                {
+                    if (Variable.Name != null)
+                        return Variable.Name;
+
+                    return Parent?.Children.IndexOf(this).ToString() ?? string.Empty;
+                }
+                set
+                {
+                    if (Variable.Name != null) Variable.Name = value;
+                }
+            }
+
+            public string Value => Variable.ToString();
+
+            public string Type => Variable.Type;
+
+            public int ChildCount => Children.Count;
+
+            public List<VariableListNode> Children { get; set; }
+            public VariableListNode Parent { get; set; }
+            public IEditableVariable Variable { get; set; }
+
+    }
+}
